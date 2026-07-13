1a5a629eac37c2c74f9f64ee90b24d84c4c211fb
tawchifulislam <tawchif04@gmail.com>
2026-07-13 18:39:26 +0600

    update auth-client.js

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,9 @@
 import { createAuthClient } from 'better-auth/react';
+import { jwtClient } from 'better-auth/client/plugins';
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+  plugins: [jwtClient()],
 });
 
 export const { useSession, signIn, signUp, signOut } = authClient;
